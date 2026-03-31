2ccd262c81bf95bb6eabf5e9e0429c8d62a9bf27
ashwani <ashwaniatulbishnoi2900@gmail.com>
2026-03-31 18:18:29 +0530

    built in function+ custom method

---
+++ arrays/searching/linear_search.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int search(int arr[], int n, int x) {
+    
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]== x)
+        {
+            return i;
+        }
+    }
+  
+    return -1;
+    
+}
+
+int main()
+{
+
+    int arr[] = {1, 2, 3, 4};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int x = 3;
+
+    cout<<search(arr,n,x);
+    
+
+}
