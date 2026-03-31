bf7a0a07e65a6240133bf909f952775c3d4f5870
ashwani <ashwaniatulbishnoi2900@gmail.com>
2026-03-31 18:08:28 +0530

    built in function

---
+++ arrays/deletion/from_ending.cpp
@@ -0,0 +1,14 @@
+// built in function
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<int> v = {10,20,30,40};
+    v.pop_back();
+    for(int i=0;i<v.size();i++)
+    {
+        cout<<v[i]<<" ";
+
+    }
+}
