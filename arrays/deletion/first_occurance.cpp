b7906d102bb423cbc7b7b85177afb0f02750023a
ashwani <ashwaniatulbishnoi2900@gmail.com>
2026-03-31 17:22:15 +0530

    brute force + correct approach + built in function

--- arrays/deletion/first_occurance.cpp
+++ arrays/deletion/first_occurance.cpp
@@ -71,3 +71,34 @@ int main()
             cout << v[i] << " ";
     }
 }
+
+// built - in function
+
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    vector<int> arr = { 10, 20, 20, 20, 30 };
+    int ele = 20;
+
+    cout << "Array before deletion\n";
+    for (int i = 0; i < arr.size(); i++)
+        cout << arr[i] << " ";
+	
+    // Find the element in the array
+    auto it = find(arr.begin(), arr.end(), ele); // it returns iterator
+    
+    // Remove the element if it is present in array
+    if (it != arr.end()) {
+        arr.erase(it);  
+    }
+    
+    cout << "\nArray after deletion\n";
+    for (int i = 0; i < arr.size(); i++)
+        cout << arr[i] << " ";
+
+    return 0;
+}
