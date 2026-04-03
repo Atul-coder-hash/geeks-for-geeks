6cdbfd4e9de28614cfc41e3d0064d3341a7df44b
ashwani <ashwaniatulbishnoi2900@gmail.com>
2026-04-03 15:52:50 +0530

    remove duplicate in array

--- arrays/problems/Remove_Duplicates_from_Sorted.cpp
+++ arrays/problems/Remove_Duplicates_from_Sorted.cpp
@@ -1,9 +1,10 @@
+// brute force 
 #include<bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-    vector<int> arr = {2, 2, 2, 2, 2};
+    vector<int> arr = {1, 2, 2, 3, 4, 4, 4, 5, 5};
     vector<int> res;
    
 
@@ -14,13 +15,67 @@ int main()
                 res.push_back(arr[i]);
 
             }
-        
-
     }
+    for(int i=0;i<res.size();i++)
+    {
+        cout<<res[i]<<" ";
+    }
+}
+
+// correct version of brute force
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<int> arr = {1, 2, 2, 3, 4, 4, 4, 5, 5};
+    vector<int> res;
+   
+
+    for(int i=0;i<arr.size()-1;i++)
+    {
+          if(arr[i]!=arr[i+1]) // check with next element agr next element previous ka equal nhi ha to result ma element add kr do
+            {
+                res.push_back(arr[i]);
+
+            }
+    }
+
+    // Add last element manually
+    res.push_back(arr[arr.size()-1]);
+
     for(int i=0;i<res.size();i++)
     {
         cout<<res[i]<<" ";
     }
 
 
+}
+
+//optinal approach for interview
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<int> arr = {1, 2, 2, 3, 4, 4, 4, 5, 5};
+    vector<int> res;
+
+    res.push_back(arr[0]); // add first element manually
+
+    for(int i=1;i<arr.size();i++)
+    {
+        if(arr[i]!=arr[i-1])
+        {
+            res.push_back(arr[i]);
+        }
+    }
+
+       for(int i=0;i<res.size();i++)
+    {
+        cout<<res[i]<<" ";
+    }
+    
 }
