92e7883fad194d39d36e5c2a3bea19776b9514c1
ashwani <ashwaniatulbishnoi2900@gmail.com>
2026-04-06 22:00:06 +0530

    juggling algorithm

--- arrays/problems/rotate_array.cpp/03juggling_algorithm.cpp
+++ arrays/problems/rotate_array.cpp/03juggling_algorithm.cpp
@@ -7,8 +7,32 @@ void rotate(vector<int> arr,int d)
 
  d%=n; // if d>n
 
- int cycle = __gcd(n,d);
- cout<<cycle;
+ int cycle = __gcd(n,d); // Calculate the number of cycles in the rotation
+ 
+ for(int i=0;i<2;i++)
+ {
+
+    int currIdx = i;// Start index of current cycle
+    int currEle = arr[currIdx];
+
+    // Rotate elements till we reach the start of cycle
+    do{
+        int nextIdx = (currIdx+d)%n;
+        int nextEle = arr[nextIdx];
+
+        arr[nextIdx] = currEle;// Update the element at next index with the current element
+        currEle = nextEle;// Update the current element to next element
+
+        currIdx = nextIdx;  // Move to the next index
+    }
+    while(currIdx!=i);
+ }
+
+ for(int i=0;i<n;i++)
+ {
+    cout<<arr[i]<<" ";
+ }
+
 
 }
 
