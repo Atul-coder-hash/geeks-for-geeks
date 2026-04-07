b276022c5c0f9365abc076ad2206c87d97a8a539
ashwani <ashwaniatulbishnoi2900@gmail.com>
2026-04-07 17:47:51 +0530

    using temporary array

---
+++ arrays/problems/07Move_all_Zeros_to_End_of_Array/temporary_array.cpp
@@ -0,0 +1,41 @@
+    #include<bits/stdc++.h>
+    using namespace std;
+
+    void moveszeroes(vector<int> arr)
+    {   int n = arr.size();
+        vector<int> temp(n,0); // give asize of arrray equal to original array and fill zero value in array
+
+        
+        int j=0;
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]!=0) // elements is not equal to zero then add element in temporary array
+            {
+                temp[j++]=arr[i];
+                
+            }
+            else{
+                continue;
+            }
+        }
+
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=temp[i];      // copy the temp array into the original array
+        
+        }
+        for(int i=0;i<n;i++)
+        {
+            cout<<arr[i]<<" ";
+        
+        }
+
+
+    }
+
+    int main()
+    {
+        vector<int> arr= {1, 2, 0, 4, 3, 0, 5, 0};
+        
+        moveszeroes(arr);
+    }
